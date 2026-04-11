a82f465e663be8625dabddc4edae977e54257ecb
saminaoum2022-png <saminaoum2022@gmail.com>
2026-04-11 20:41:58 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         <title>Nabad AI</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#00D4FF" />
+        <meta name="theme-color" content="#f7f8fc" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Nabad AI" />
