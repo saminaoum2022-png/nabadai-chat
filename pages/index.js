afbaafc8bc244af13345fe1af6e5574309cc82c8
saminaoum2022-png <saminaoum2022@gmail.com>
2026-04-16 22:19:59 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -123,7 +123,7 @@ export default function Home() {
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="NabadAi" />
         <link rel="icon" href="/logo.png" />
-        <link rel="apple-touch-icon" href="/logo.png" />
+        <link rel="apple-touch-icon" href="/logo-icon.png" />
         <link rel="manifest" href="/manifest.json" />
 
         {/* [FIX-7] Open Graph & Twitter meta tags */}
