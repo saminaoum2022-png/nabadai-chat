cbb78efb7935bb2ba6abf0bcd8b59f7bcf69a533
saminaoum2022-png <saminaoum2022@gmail.com>
2026-04-21 00:09:06 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -167,7 +167,7 @@ export default function Home() {
 
       <Script
         id="nabad-widget-script"
-        src="/widget.js?v=42"
+        src="/widget.js?v=43"
         strategy="afterInteractive"
       />
 
