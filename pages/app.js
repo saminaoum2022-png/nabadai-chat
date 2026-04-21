b17fff60fc70214e450531ad1c54009433c171d2
saminaoum2022-png <saminaoum2022@gmail.com>
2026-04-21 14:15:50 +0400

    Update app.js

--- pages/app.js
+++ pages/app.js
@@ -65,7 +65,7 @@ export default function AppPage() {
           };
         `}
       </Script>
-      <Script id="nabad-widget-script-app" src="/widget.js?v=51" strategy="afterInteractive" />
+      <Script id="nabad-widget-script-app" src="/widget.js?v=52" strategy="afterInteractive" />
 
       <main className="nabad-app-page">
         <div className="nabad-app-shell">
